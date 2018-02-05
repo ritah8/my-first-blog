46aea8976d26e4d90658f20b8de56f39fa176a32
ritah <eshiwanin@gmail.com>
2018-02-05 13:01:12 +0300

    saved changes made onhtml

--- blog/urls.py
+++ blog/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.conf.urls import include
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import url
+from . import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'', include('blog.urls'))
-]
+    url(r'^$', views.post_list, name='post_list'),
+    ]
+
